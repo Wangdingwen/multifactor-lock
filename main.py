ed64fd0231db8e7b24e7f9ca7c63c9f863df5f94
王鼎文 <15158879870@139.com>
2018-08-17 21:45:24 +0800

    Optimization

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ while True:
     try:
         blue = blueclient.receive("98:D3:61:FD:51:29")
         if  blue == 'SJ3M4GZVCZTAOME7APZRPQOCQJ5VNZY4':
-            time.sleep(2)
             print('BLUETOOTH OK')
             nfcr = arduino_nfc.receive_auth_all('98:D3:21:FC:81:0F')
             if nfcr >= 0:
